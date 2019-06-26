7eef031d4e499d93578c72520c6b5dd18e338474
Kelvin Chung <kelvincc@cs.washington.edu>
2019-06-26 16:57:42 -0700

    Basic Player mechanics
    Image sprites added:
    HUB BALLROOM
    PLAYER 1 (MITCH MCCONNELL)
    CHASER (AMERICAN VOTERS)
    
    Added Chaser and PlayerOne script

---
+++ Assets/Scripts/PlayerGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGeneric : MonoBehaviour
+{
+    public float speed;
+    public int jumpHeight;
+    // Start is called before the first frame update
+    public void Start()
+    {
+      speed = 5;
+      jumpHeight = 7;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+      transform.Translate(transform.right * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerOne.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOne : PlayerGeneric
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        }
+        base.Update();
+    }
+}
