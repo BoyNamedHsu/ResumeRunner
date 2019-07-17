7a4747997a5c0fd456f684beb30a1c6bcfa0e5da
Kelvin Chung <kelvincc@cs.washington.edu>
2019-07-16 17:16:40 -0700

    Created obstacle generation + destruction
    Also created superclass "Generator" and subclasses ObstacleGenerator and Platform Generator.

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    // The Transform coord point if the camera passes it, we generate blocks
+    public Transform generationPoint;
+
+    protected float platformWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
